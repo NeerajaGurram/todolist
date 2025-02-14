769274e2360ce2dcd2a0ef2a6f71152e0481783b
Neerajagurram <152693538+NeerajaGurram@users.noreply.github.com>
2025-02-15 00:04:17 +0530

    Update page.jsx
    cancel functionality

--- app/add-task/page.jsx
+++ app/add-task/page.jsx
@@ -28,29 +28,37 @@ export default function AddTask() {
       console.error('Error adding task:', error);
     }
   };
+  const handleCancel = () => {
+    router.push('/home');
+  };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Add Task</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="container mx-auto p-4 ">
+      <h1 className="text-2xl font-bold mb-4 lg:mx-20">Add Task</h1>
+      <form onSubmit={handleSubmit} className='lg:mx-20'>
         <input
           type="text"
           placeholder="Title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="border p-2 w-full mb-4"
+          className="border border-gray-400 p-2 w-full mb-4"
           required
         />
         <textarea
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="border p-2 w-full mb-4"
+          className="border border-gray-400 p-2 w-full mb-4"
           required
         />
+        <div className='flex space-x-4'>
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
           Add Task
         </button>
+        <button onClick={handleCancel} className="bg-blue-500 text-white px-4 py-2 rounded">
+          Cancel
+        </button>
+        </div>
       </form>
     </div>
   );
